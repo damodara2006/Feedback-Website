6fabb852847a1bd3b29a586a55a781a8fba55478
damodara2006 <damodara2006@gmail.com>
2025-04-26 16:59:46 +0530

    fix: Correct URL format in CORS configuration for production server

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ import express from "express"
 import cors from "cors"
 const app = express()
 app.use(cors({
-    origin:['http://localhost:5173','https://680cb75b50e613e32d245900--idyllic-gelato-6f272a.netlify.app'],
+    origin:['http://localhost:5173','https://680cbb6d706ffc0008dd2aa5--idyllic-gelato-6f272a.netlify.app/'],
     credentials:true
 }))
 app.use(express.json())
